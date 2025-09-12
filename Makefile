fecbfcc4715e8d8a8100ddbd7e1af77d0b44ec78
Konrad Wasowicz <exaroth@gmail.com>
2025-09-12 21:17:40 +0200

    Add AppImage build instructions

--- Makefile
+++ Makefile
@@ -10,11 +10,14 @@ build:
 build-release:
 	zig build --global-cache-dir ./vendor -Dtarget=x86_64-linux-musl
 
-.PHONY: build-appimage
-build-appimage:
-	docker container run -v $(pwd):/build appimagecrafters/appimage-builder appimage-builder --recipe=/build/AppImageBuilder.yml
 
 .PHONY: random-config
 random-config:
 	$(eval CONFIG_NAME = $(shell mktemp -u XXXXXX))
 	./zig-out/bin/i3_news -a $(CONFIG_NAME)
+
+.PHONY: build-appimage
+build-appimage:
+	./scripts/buildappimage.sh
+
+
---
+++ scripts/buildappimage.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -f ./tmp/appimagetool.AppImage ]; then
+    echo "appimagetool already exists, skipping download..."
+else
+	curl -# -L -o ./tmp/appimagetool.AppImage https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+    chmod +x ./tmp/appimagetool.AppImage 
+fi
+
+zig build --global-cache-dir ./vendor -Dtarget=x86_64-linux-musl
+
+mkdir -p build
+cp -Rf AppDir build
+mkdir -p build/AppDir/usr/local/bin
+
+cp scripts/bscroll build/AppDir/usr/local/bin
+cp zig-out/bin/i3_news build/AppDir/usr/local/bin
+cp VERSION build/AppDir
+wget -O build/AppDir/usr/local/bin/zscroll https://raw.githubusercontent.com/noctuid/zscroll/master/zscroll
+chmod +x build/AppDir/usr/local/bin/zscroll
+
+./tmp/appimagetool.AppImage ./build/AppDir ./build/i3_news.AppImage
+
