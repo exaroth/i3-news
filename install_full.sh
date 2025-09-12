0d8e6993d7d569a4fa722cdc9f06844c5eb6cbcc
Konrad Wasowicz <exaroth@gmail.com>
2025-09-12 15:32:35 +0200

    Create new release for tags in ci

--- install_full.sh
+++ install_full.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-curl -# -L -o /tmp/i3_news https://github.com/exaroth/i3-news/releases/download/latest/i3_news_self_contained
+curl -# -L -o /tmp/i3_news https://github.com/exaroth/i3-news/releases/download/stable/i3_news_self_contained
 chmod +x /tmp/i3_news
 sudo mv /tmp/i3_news /usr/local/i3_news
 echo "i3_news installed at /usr/local/bin/i3_news\n"
