d7d9031f307bfc46eb3c420ad35c5bda7204fd1c
Konrad Wasowicz <exaroth@gmail.com>
2025-10-17 03:56:08 +0200

    Fix i3 news bin name in scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-curl -# -L -o /tmp/i3-news https://github.com/exaroth/i3-news/releases/download/stable/i3_news
+curl -# -L -o /tmp/i3-news https://github.com/exaroth/i3-news/releases/download/stable/i3-news
 chmod +x /tmp/i3-news
 sudo mv /tmp/i3-news /usr/local/bin/i3-news
-echo "i3-news installed at /usr/local/bin/i3_news"
+echo "i3-news installed at /usr/local/bin/i3-news"
